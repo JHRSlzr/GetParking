0b328f0fec089a243e9f564b9df480fa404d63a4
JHRSlzr <hernanryz@hotmail.com>
2021-06-17 23:44:16 -0500

    firebase

--- conn.js
+++ conn.js
@@ -1,14 +1,14 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyArvJbOGzW97CmNwmhJuYZXcUwCSJU40nU",
-    authDomain: "eats-11396.firebaseapp.com",
-    databaseURL: "https://eats-11396-default-rtdb.firebaseio.com",
-    projectId: "eats-11396",
-    storageBucket: "eats-11396.appspot.com",
-    messagingSenderId: "406752767746",
-    appId: "1:406752767746:web:86cb8652c2aae017161c1b"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyD82iSnsFOIMpSsdFnAOxkp4oPhk5VZNgQ",
+  authDomain: "cards-c3e11.firebaseapp.com",
+  databaseURL: "https://cards-c3e11-default-rtdb.firebaseio.com",
+  projectId: "cards-c3e11",
+  storageBucket: "cards-c3e11.appspot.com",
+  messagingSenderId: "837302697098",
+  appId: "1:837302697098:web:7081dce1586fe72b3ac173"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 
